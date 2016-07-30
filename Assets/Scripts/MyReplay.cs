a429dec13f735984aa3729eddf0c349ab6bb5ee1
Miguel Torres <torresCodes@icloud.com>
2016-07-29 20:19:18 -0700

    Designing a Replay System
    Replay class and MyKeyFrame Struct

---
+++ Assets/Scripts/MyReplay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Replay	: MonoBehaviour	{
+
+	//Use this for initialization
+	void Star(){
+	
+	}
+
+	//Update is called once per frame
+	void Update()	{
+	
+	}
+}
+
+//helper class for replay class
+public struct MyKeyFrame	{
+	
+	public float frameTime;
+	public Vector3 position;
+	public float frame;
+	Quaternion rotation;
+
+	//constructor for MyKeyFrame
+	public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation)	{
+		frameTime = aTime;
+		position = aPosition;
+		rotation = aRotation;
+	}
+}
